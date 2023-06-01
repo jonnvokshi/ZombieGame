15bb67215ffd33e8923e6e794a13a427eb7ca747
Flamurzenelaj <flamurzenelaj6@gmail.com>
2023-06-01 03:17:20 +0200

    Added the function to spawn enemies at the top of the screen

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import * as PIXI from "pixi.js";
-//import Victor from "victor";
-//import Matter from "matter-js";
+import Victor from "victor";
+
 
 let canvasSize = 256;
 const canvas = document.getElementById("mycanvas");
@@ -20,8 +20,29 @@ square.tint = 0xea985d;
 
 app.stage.addChild(square);
 
+let enemyRadius = 16;
+const enemy = new PIXI.Graphics();
+let r = randomSpawnPoint();
+enemy.position.set(r.x, r.y);
+enemy.beginFill(0xFF0000, 1);
+enemy.drawCircle(0, 0, enemyRadius);
+enemy.endFill();
+app.stage.addChild(enemy);
+
 app.ticker.add((delta) => {
     const cursorPosition = app.renderer.plugins.interaction.mouse.global;
     let angle = Math.atan2(cursorPosition.y - square.position.y, cursorPosition.x - square.position.x) + Math.PI / 2;
     square.rotation = angle;
 })
+
+function randomSpawnPoint() {
+    let edge = Math.floor(Math.random() * 4);
+    let spawnPoint = new Victor(0, 0)
+    switch (edge) {
+        case 0: spawnPoint.x = canvasSize * Math.random();
+            break;
+        default:
+            break;
+    }
+    return spawnPoint;
+}
